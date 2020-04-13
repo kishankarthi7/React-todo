cd5399238240cd394b538943b6c58db11b64594f
KISHAN L <kishan.karthi7@gmail.com>
2020-04-14 01:27:08 +0200

    adding todoitems

--- src/App.js
+++ src/App.js
@@ -22,15 +22,55 @@ export default class App extends React.Component {
     });
   };
 
+  updateNewTextValue = (event) => {
+    this.setState({
+      newItemText: event.target.value,
+    });
+  };
+
+  createNewTodo = () => {
+    if (
+      !this.state.todoItems.find(
+        (item) => item.action === this.state.newItemText,
+      )
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          { action: this.state.newItemText, done: false },
+        ],
+        newItemText: '',
+      });
+    }
+  };
+
   render() {
     return (
       <div>
-        <h4 className="bg-primary text-white text-center p-2">
-          {this.state.userName}'s To Do List
-        </h4>
-        <button className="btn btn-primary" onClick={this.changeStateData}>
-          Change
-        </button>
+        <div>
+          <h4 className="bg-primary text-white text-center p-2">
+            {this.state.userName}'s To Do List (
+            {this.state.todoItems.filter((t) => !t.done).length} items to do)
+          </h4>
+          <button className="btn btn-primary" onClick={this.changeStateData}>
+            Change
+          </button>
+        </div>
+        <div className="container-fluid">
+          <div className="my-1">
+            <input
+              className="form-control"
+              value={this.state.newItemText}
+              onChange={this.updateNewTextValue}
+            />
+            <button
+              className="btn btn-primary mt-1"
+              onClick={this.createNewTodo}
+            >
+              Add
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
