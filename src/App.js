f9727a36c70f1038214048e8ee3a7fbc05f2de67
KISHAN L <kishan.karthi7@gmail.com>
2020-04-20 12:07:26 +0200

    add new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import TodoBanner from './TodoBanner';
+import TodoCreator from './TodoCreator';
+import TodoRow from './TodoRow';
 
-export default class App extends React.Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -9,99 +11,47 @@ export default class App extends React.Component {
       todoItems: [
         { action: 'Buy Flowers', done: false },
         { action: 'Get Shoes', done: false },
-        { action: 'collect Tickets', done: true },
-        { action: 'Call joe', done: false },
+        { action: 'Collect Tickets', done: true },
+        { action: 'Call Joe', done: false },
       ],
-      newItemText: '',
+      //newItemText: ""
     };
   }
-
-  changeStateData = () => {
-    this.setState({
-      userName: this.state.userName === 'Adam' ? 'Bob' : 'Adam',
-    });
-  };
-
   updateNewTextValue = (event) => {
-    this.setState({
-      newItemText: event.target.value,
-    });
+    this.setState({ newItemText: event.target.value });
   };
-
-  createNewTodo = () => {
-    if (
-      !this.state.todoItems.find(
-        (item) => item.action === this.state.newItemText,
-      )
-    ) {
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find((item) => item.action === task)) {
       this.setState({
-        todoItems: [
-          ...this.state.todoItems,
-          { action: this.state.newItemText, done: false },
-        ],
-        newItemText: '',
+        todoItems: [...this.state.todoItems, { action: task, done: false }],
       });
     }
   };
-
   toggleTodo = (todo) =>
     this.setState({
       todoItems: this.state.todoItems.map((item) =>
         item.action === todo.action ? { ...item, done: !item.done } : item,
       ),
     });
-
   todoTableRows = () =>
     this.state.todoItems.map((item) => (
-      <tr key={item.action}>
-        <td>{item.action}</td>
-        <td>
-          <input
-            type="checkbox"
-            checked={item.done}
-            onChnage={() => this.toggleTodo(item)}
-          />
-        </td>
-      </tr>
+      <TodoRow key={item.action} item={item} callback={this.toggleTodo} />
     ));
-
-  render() {
-    return (
-      <div>
-        <div>
-          <h4 className="bg-primary text-white text-center p-2">
-            {this.state.userName}'s To Do List (
-            {this.state.todoItems.filter((t) => !t.done).length} items to do)
-          </h4>
-          <button className="btn btn-primary" onClick={this.changeStateData}>
-            Change
-          </button>
-        </div>
-        <div className="container-fluid">
-          <div className="my-1">
-            <input
-              className="form-control"
-              value={this.state.newItemText}
-              onChange={this.updateNewTextValue}
-            />
-            <button
-              className="btn btn-primary mt-1"
-              onClick={this.createNewTodo}
-            >
-              Add
-            </button>
-          </div>
-          <table className="table table-stripped table-bordered">
-            <thead>
-              <tr>
-                <th>Description</th>
-                <th>Done</th>
-              </tr>
-            </thead>
-            <tbody>{this.todoTableRows()}</tbody>
-          </table>
-        </div>
+  render = () => (
+    <div>
+      <TodoBanner name={this.state.userName} tasks={this.state.todoItems} />
+      <div className="container-fluid">
+        <TodoCreator callback={this.createNewTodo} />
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoTableRows()}</tbody>
+        </table>
       </div>
-    );
-  }
+    </div>
+  );
 }
